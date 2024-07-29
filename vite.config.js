5432722dabdcb3762c754f38d62884ede17c93af
esxr <dhoolia.pranav@gmail.com>
2024-07-29 12:18:15 +1000

    fix(dist): fix jsDelivr

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // name the file ourselves because it is going to be referred
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: 'index.js',
+        assetFileNames: 'index.css',
+      },
+    },
+  },
 })
